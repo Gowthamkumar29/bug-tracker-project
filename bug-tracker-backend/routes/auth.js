b8147ab9491ae63f82c20f61bab89724a4280231
Gowthamkumar29 <gk02k5@gmail.com>
2025-07-18 00:41:37 +0530

    auth.js

--- bug-tracker-backend/routes/auth.js
+++ bug-tracker-backend/routes/auth.js
@@ -6,20 +6,20 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-// Register a new user
+
 router.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    // Check if user already exists
+    // email
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: 'User already exists' });
 
-    // Hash password
+    //password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Create new user
+  //user
     const newUser = new User({
       username,
       email,
@@ -27,7 +27,7 @@ router.post('/register', async (req, res) => {
     });
 
     await newUser.save();
-
+// status
     res.status(201).json({ message: 'User registered successfully' });
 
   } catch (err) {
@@ -36,20 +36,20 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// Login user
+// login
 router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check if user exists
+    
     const user = await User.findOne({ email });
     if (!user) return res.status(404).json({ message: 'User not found' });
 
-    // Compare password
+   
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
 
-    // Create JWT token
+    
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: '1d',
     });
